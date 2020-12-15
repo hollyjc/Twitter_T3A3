79ea693b78d074b1fc5a3e90b451c0d8b4bc3432
Holly <holly_cooper@live.com.au>
2020-12-15 19:46:30 +1000

    app

--- App/app.py
+++ App/app.py
@@ -4,4 +4,16 @@ import time
 
 class TwitterBot:
     def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Firefox()
         
+    def login(self):
+        bot = self.bot
+        bot.get('https://twitter.com/')
+        time.sleep(3)
+
+
+
+holly = TwitterBot('email', 'password')
+holly.login()
