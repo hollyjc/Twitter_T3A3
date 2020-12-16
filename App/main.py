021da3818daa87926af215fc38776764cd16c801
Holly <holly_cooper@live.com.au>
2020-12-16 21:52:14 +1000

    requirements

--- App/main.py
+++ App/main.py
@@ -1,8 +1,18 @@
+import http.server
+import socketserver
+
+PORT = 8000
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def try_app():
-    return 'Trying and crying' 
+    print("time to marry rich") 
 
 #######3
