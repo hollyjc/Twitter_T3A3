4bba8c1423dc8e39d0d82f6fad51a1775a60fc3d
Holly <holly_cooper@live.com.au>
2020-12-16 13:52:48 +1000

    changes

--- App/app.py
+++ App/app.py
@@ -15,8 +15,18 @@ class TwitterBot:
         bot.get('https://twitter.com/')
         time.sleep(3)
         email = bot.find_element_by_class_name('email-input')
+        twitter_password = bot.find_element_by_name('session[password]')
+        email.clear()
+        password.clear()
+        email.send_keys(self.usermame)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        time.sleep(3)
+    
+    def like_tweet(self,hashtag):
+        bot = self.bot
+        botbot.get('https://twitter.com/search?q=' + hash '%23otter&src=typed_query')
 
 
-
-holly = TwitterBot('email', 'password')
-holly.login()
+otter_twitter= TwitterBot('email', 'password')
+otter_twitter.login()
